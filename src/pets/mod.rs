c22f16692224ed393611c406ecb42a7a2bf899a5
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-04 22:00:36 -0700

    more animals

--- src/pets/mod.rs
+++ src/pets/mod.rs
@@ -60,3 +60,59 @@ impl Pet for Cat {
         }
     }
 }
+
+pub struct Fox {
+    variant: AtomicU8
+}
+
+impl Pet for Fox {
+    #[cfg(not(feature="random"))]
+    fn dialog_got_here(&self, location: &str) -> String {
+        if self.variant.fetch_add(1, Ordering::Relaxed) % 2u8 == 0 {
+            format!("?? at {}!", location)
+        } else {
+            format!("?? at {}!", location)
+        }
+    }
+    #[cfg(feature="random")]
+    fn dialog_got_here(&self, location: &str) -> String {
+        if self.variant.fetch_add(random_range(0u8..4u8), Ordering::Relaxed) % 2u8 == 0 {
+            format!("?? at {}!", location)
+        } else {
+            format!("?? at {}!", location)
+        }
+    }
+    fn new() -> Self {
+        Self {
+            variant: AtomicU8::new(0)
+        }
+    }
+}
+
+pub struct Cow {
+    variant: AtomicU8
+}
+
+impl Pet for Cow {
+    #[cfg(not(feature="random"))]
+    fn dialog_got_here(&self, location: &str) -> String {
+        if self.variant.fetch_add(1, Ordering::Relaxed) % 2u8 == 0 {
+            format!("Moo at {}!", location)
+        } else {
+            format!("Bell rang at {}!", location)
+        }
+    }
+    #[cfg(feature="random")]
+    fn dialog_got_here(&self, location: &str) -> String {
+        if self.variant.fetch_add(random_range(0u8..4u8), Ordering::Relaxed) % 2u8 == 0 {
+            format!("Moo at {}!", location)
+        } else {
+            format!("Bell rang at {}!", location)
+        }
+    }
+    fn new() -> Self {
+        Self {
+            variant: AtomicU8::new(0)
+        }
+    }
+}
