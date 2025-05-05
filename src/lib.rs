fc45e8c7457102a2ea595bcd283606e6d51b1e0e
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-04 22:18:09 -0700

    better docs, reordered methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+/// Debug pets is a crate that adds the cutest debugging tools you have ever seen.
+/// 
+/// Example usage:
+/// 
+/// ```
+/// use debug_pets::prelude::*;
+/// 
+/// static DOG: PetDebugger<pets::dog, DefaultFormatter> = PetDebugger::new()
+/// 
+/// fn main() {
+///     DOG.got_here("main function")
+/// }
+/// ```
+/// 
+/// Console output can vary. Possible console output:
+/// 
+/// ```txt
+/// $ cargo run
+/// Barked at main function!
+/// ```
+/// 
+/// Contains many other animals for you to explore!
+
 pub mod prelude;
 pub mod pets;
 pub mod printer;
@@ -9,16 +32,6 @@ use printer::Printer;
 pub struct PetDebugger<P: Pet, R: Printer> {
     lazy: LazyLock<OpenDebugger<P>>,
     _p: PhantomData<R>
-}
-
-pub struct OpenDebugger<P: Pet> {
-    pet: P
-}
-
-impl<P: Pet> OpenDebugger<P> {
-    pub fn dailog_got_here(&self, location: &str) -> String {
-        self.pet.dialog_got_here(location)
-    }
 }
 
 impl<P: Pet, R: Printer> PetDebugger<P, R> {
@@ -38,6 +51,16 @@ impl<P: Pet, R: Printer> Deref for PetDebugger<P, R> {
     type Target = OpenDebugger<P>;
     fn deref(&self) -> &Self::Target {
         return &*self.lazy;
+    }
+}
+
+pub struct OpenDebugger<P: Pet> {
+    pet: P
+}
+
+impl<P: Pet> OpenDebugger<P> {
+    pub fn dailog_got_here(&self, location: &str) -> String {
+        self.pet.dialog_got_here(location)
     }
 }
 
