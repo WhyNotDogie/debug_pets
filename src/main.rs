7c687d4217fa51b7ac814360b89d99b996f9bfa3
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-05-04 22:11:49 -0700

    no stupid main.rs

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use debug_pets::prelude::*;
-
-static DOG: PetDebugger<pets::Dog, DefaultPrinter> = PetDebugger::new();
-
-fn main() {
-    for _ in 1..10 {
-        DOG.got_here("cheese");
-    }
-}
